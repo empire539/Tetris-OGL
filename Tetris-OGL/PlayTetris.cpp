50c907c10ba84f21719e2b960f8b2968d92bd8fc
empire539 <vic9317@gmail.com>
2013-03-20 00:27:45 -0400

    Beginning source files and documenation added.

---
+++ Tetris-OGL/PlayTetris.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+
+/**
+ * The application entry point.
+ */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	try {
+		// TODO: fill in WinMain code
+	}
+	catch (Exception e) {
+		// TODO: fill in exception handling code
+	}
+
+	return 0;
+}
